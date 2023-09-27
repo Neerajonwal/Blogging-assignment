ce93ca7a84c6b976e8dad0d8263c27d53a4b7b85
Neerajonwal <jonwalneeraj2@gmail.com>
2023-09-28 00:58:46 +0530

    config file

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    Port: 3090,
+    Connection_Sting: "mongodb://localhost:27017/BlogPost", // can update db string with your personal string
+    jwtSecret: "hvsggftwsft345"
+}
